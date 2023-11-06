50a6bf68efdbb0ef13bfe5ae73a00dd2f24f1743
Shahreyar Tonmoy <shahreyartonmoy001@gmail.com>
2023-11-06 16:49:20 +0600

    updata section done

--- index.js
+++ index.js
@@ -30,13 +30,14 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+       
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+       
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         const AddassignmentCollaction = client.db("GroupStudy").collection('Addassignment')
-        // const MyCartCollaction = client.db("AyenTraders").collection('mycart')
+        const SubmitassignmentCollaction = client.db("GroupStudy").collection('SubmitAssignment')
+        
         // const BillsCollaction = client.db("AyenTraders").collection('bills')
 
 
@@ -47,20 +48,32 @@ async function run() {
             res.send(result)
             console.log(result);
         })
+        app.post('/submit' , async (req, res) => {
+            const addProduct = req.body
+            console.log(addProduct);
+            const result = await SubmitassignmentCollaction.insertOne(addProduct)
+            res.send(result)
+            console.log(result);
+        })
 
         app.get('/addassignment', async (req, res) => {
             const cursor = AddassignmentCollaction.find()
             const result = await cursor.toArray()
             res.send(result)
         })
+        app.get('/submit', async (req, res) => {
+            const cursor = SubmitassignmentCollaction.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
         
-        // app.get('/items/:id' , async (req, res) => {
-        //     const id = req.params.id
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await itemsCollaction.findOne(query)
-        //     res.send(result)
+        app.get('/addassignment/:id' , async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await AddassignmentCollaction.findOne(query)
+            res.send(result)
 
-        // })
+        })
         // app.get('/mycart' , async (req, res) => {
         //     const cursor = MyCartCollaction.find()
         //     const result = await cursor.toArray()
@@ -114,20 +127,27 @@ async function run() {
 
         // // // update opction
 
-        // app.put('/mycart/:id' , async (req, res) => {
-        //     const id = req.params.id
-        //     const filter = { _id: new ObjectId(id) }
-        //     const options = { upsert: true }
-        //     const UpdateProduct = req.body
-        //     const Updates = {
-        //         $set: {
-        //             price: UpdateProduct.price,
+        app.put('/addassignment/:id' , async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const UpdateProduct = req.body
+            const Updates = {
+                $set: {
+                   
+                    Title: UpdateProduct.Title,
+                    AssignmentDifficulty: UpdateProduct.AssignmentDifficulty,
+                    Marks: UpdateProduct.Marks,
+                    Description: UpdateProduct.Description,
+                    Date: UpdateProduct.Date,
+                    email: UpdateProduct.email,
+                    ImageURL: UpdateProduct.ImageURL,
                     
-        //         }
-        //     }
-        //     const result = await MyCartCollaction.updateOne(filter, Updates, options)
-        //     res.send(result)
-        // })
+                }
+            }
+            const result = await AddassignmentCollaction.updateOne(filter, Updates, options)
+            res.send(result)
+        })
 
      
         
