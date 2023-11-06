2edba2f805301d978524fa8d16278f7b3958b9f2
Shahreyar Tonmoy <shahreyartonmoy001@gmail.com>
2023-11-06 19:57:27 +0600

    assignment submit done

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
             res.send(result)
         })
         
+        app.get('/submit/:id' , async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await SubmitassignmentCollaction.findOne(query)
+            res.send(result)
+
+        })
         app.get('/addassignment/:id' , async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
